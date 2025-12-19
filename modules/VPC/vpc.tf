047b9ee30e7d93c50752bde8cfdd3457aea42e02
medurubharath <medurubharathstvd@gmail.com>
2025-12-19 01:26:42 -0600

    updating the vpc source url

--- modules/VPC/vpc.tf
+++ modules/VPC/vpc.tf
@@ -1,5 +1,5 @@
 module "vpc" {
-    #source = "../custom_templates/aws_vpc"
+    #source = "../custom_templates/aws_vpc" # Uncomment this line if you are using a local custom template
     source = "git::https://github.com/DevOpsSkillUp/Terraform.git//modules/custom_templates/aws_vpc?ref=main"
     project_name = var.project_name
     environment = var.environment
