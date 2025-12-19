72c159cff32bd27353453a49ffe20f6c769543ff
medurubharath <medurubharathstvd@gmail.com>
2025-12-19 02:57:08 -0600

    commands added

--- modules/custom_templates/aws_vpc/data.tf
+++ modules/custom_templates/aws_vpc/data.tf
@@ -7,10 +7,10 @@ data "aws_vpc" "default" {
   default = true
 }
 
-data "aws_route_table" "main" {
+data "aws_route_table" "main" {   #data source default vpc route table
   vpc_id = local.default_vpc_id
   filter {
-    name = "association.main"
+    name = "association.main"  # which is the ROUTE TABLE associated with the MAIN COLUMN NAME
     values = ["true"]
   }
 }
--- modules/custom_templates/aws_vpc/peering.tf
+++ modules/custom_templates/aws_vpc/peering.tf
@@ -1,4 +1,4 @@
-resource "aws_vpc_peering_connection" "default" {
+resource "aws_vpc_peering_connection" "default" { #this is for vpc peering connection
   count = var.is_peering_required ? 1 : 0
   
   vpc_id        = aws_vpc.main.id #requestor
@@ -14,28 +14,28 @@ resource "aws_vpc_peering_connection" "default" {
   )
 }
 
-resource "aws_route" "public_peering" {
+resource "aws_route" "public_peering" { #this is for public route table
   count = var.is_peering_required ? 1 : 0
   route_table_id            = aws_route_table.public.id
   destination_cidr_block    = local.default_vpc_cidr
   vpc_peering_connection_id = aws_vpc_peering_connection.default[count.index].id  #aws_vpc_peering_connection.default.id it will give error because of count we are using list so we need to use index
 }
 
-resource "aws_route" "private_peering" {
+resource "aws_route" "private_peering" { #this is for private route table
   count = var.is_peering_required ? 1 : 0
   route_table_id            = aws_route_table.private.id
   destination_cidr_block    = local.default_vpc_cidr
   vpc_peering_connection_id = aws_vpc_peering_connection.default[count.index].id
 }
 
-resource "aws_route" "database_peering" {
+resource "aws_route" "database_peering" { #this is for database route table
   count = var.is_peering_required ? 1 : 0
   route_table_id            = aws_route_table.database.id
   destination_cidr_block    = local.default_vpc_cidr
   vpc_peering_connection_id = aws_vpc_peering_connection.default[count.index].id
 }
 
-resource "aws_route" "default_peering" {
+resource "aws_route" "default_peering" {  #this is for default route table
   count = var.is_peering_required ? 1 : 0
   route_table_id            = data.aws_route_table.main.route_table_id
   destination_cidr_block    = var.vpc_cidr
